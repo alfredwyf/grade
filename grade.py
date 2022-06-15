cdf2597d7b4f41293e06d32d586e7983e9bcfbe1
Alfred Wong <wyfalfred@gmail.com>
2022-06-14 21:45:57 -0500

    revised commit

--- grade.py
+++ grade.py
@@ -6,12 +6,12 @@ grade = int(grade)
 
 #test the range of the number and print the appropriate letter grade
 if grade >= 90:
-	print('A')
+	print('you got grade A')
 elif grade >= 80:
-	print('B')
+	print('you got grade B')
 elif grade >= 70:
-	print('C')
+	print('you got grade C')
 elif grade >= 60:
-	print('D')
+	print('you got grade D')
 else:
-	print('F')
+	print('you got grade F')
